c5de81f802c80d275eae017a6e11929c8863e76d
Danny Rizko <drizko07@gmail.com>
2016-06-01 13:10:33 -0700

    Completes googleMaps integration

---
+++ src/components/google_maps.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+export default (props) => {
+  return (
+    <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div style={{ height: "100%"}} />
+        }
+        googleMapElement={
+          <GoogleMap
+            defaultZoom={12}
+            defaultCenter={{ lat: props.lat, lng: props.lon }}
+          >
+          </GoogleMap>
+        }
+      />
+    </section>
+  );
+}
